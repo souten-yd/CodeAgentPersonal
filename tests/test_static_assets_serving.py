d7ca369862ad78c12b863182953410b29fc124b9
Souten <k.kenshirou@gmail.com>
2026-05-07 21:55:22 +0900

    test: guard static app css serving

---
+++ tests/test_static_assets_serving.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+
+import main
+from tests.helpers.ui_contract import load_root_ui_html_text
+
+
+CSS_PATH = "/static/css/app.css"
+CSS_TOKENS = (
+    ":root{",
+    "--bg:",
+    ".atlas-subview-tabs",
+    ".mob-tabs",
+)
+
+
+def test_app_css_is_served_as_css_with_expected_tokens():
+    client = TestClient(main.app)
+
+    response = client.get(CSS_PATH)
+
+    assert response.status_code == 200
+    content_type = response.headers.get("content-type", "")
+    assert "css" in content_type.lower()
+    for token in CSS_TOKENS:
+        assert token in response.text
+
+
+def test_ui_html_links_external_app_css():
+    ui_html = load_root_ui_html_text()
+
+    assert f'<link rel="stylesheet" href="{CSS_PATH}">' in ui_html
