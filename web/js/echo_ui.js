618f7ea106240ec0fbbc1472955f0acd6720f2ef
Souten <k.kenshirou@gmail.com>
2026-05-14 13:07:19 +0900

    Add UI module loader stubs and contract tests

---
+++ web/js/echo_ui.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+  const root = window;
+  const registry = root.__kasaneModules || (root.__kasaneModules = {});
+  registry.echoUi = Object.assign(registry.echoUi || {}, {
+    name: "echo_ui",
+    loaded: true,
+  });
+}());
